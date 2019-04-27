2bc6654ca4fe7b7ef554517889fbf281c7097fea
Timothy Shores <timothymshores@gmail.com>
2019-04-27 00:06:23 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Gatsby JS - The Great Gatsby Bootcamp</div>
+const Index = () => {
+    return (
+        <div>
+            <h1>Gatsby JS - The Great Gatsby Bootcamp</h1>
+            <h5>This repo is a follow along codebase to the <a href="https://youtu.be/8t0vNu2fCCM">The Great Gatsby Bootcamp</a> by Andrew Mead on YouTube</h5>
+        </div>
+    )
+}
+
+export default Index;
