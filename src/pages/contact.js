6a5f2f6484cdc8e9722aad273bf2bec5e0c21325
Timothy Shores <timothymshores@gmail.com>
2019-04-27 00:41:54 -0500

    Fixed external links in contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,9 +12,9 @@ const Contact = () => {
             <h1>Contact</h1>
             <p>Timothy Shores is a full-time full stack web development student at Lambda School. On Friday nights he enjoys staying in and watching Game of Thrones with his girlfriend and learning about GatsbyJS</p>
             <ul>
-                <li>GitHub: <Link to="https://github.com/timothyshores">@timothyshores</Link></li>
-                <li>LinkedIn: <Link to="https://www.linkedin.com/in/timothyshores/">Timothy Shores</Link></li>
-                <li>Twitter: <Link to="https://twitter.com/timothymshores">@timothymshores</Link></li>
+                <li>GitHub: <a href="https://github.com/timothyshores">@timothyshores</a></li>
+                <li>LinkedIn: <a href="https://www.linkedin.com/in/timothyshores/">Timothy Shores</a></li>
+                <li>Twitter: <a href="https://twitter.com/timothymshores">@timothymshores</a></li>
 
             </ul>
         </div>
