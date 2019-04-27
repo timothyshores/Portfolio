b8a64fc30e6b4eadc624bf900dd79284bd52834f
Timothy Shores <timothymshores@gmail.com>
2019-04-27 14:21:20 -0500

    Added styles to index.scss

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from './footer'
 import Header from './header'
-import '../styles/index.css'
+import '../styles/index.scss'
 
 const Layout = (props) => {
     return (
