35b07ef3370e573e3db1c197c0fad5344b6dfbc8
Timothy Shores <timothymshores@gmail.com>
2019-04-27 00:18:14 -0500

    Create about and contact page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Timothy Shores is a full-time full stack web development student at Lambda School. On Friday nights he enjoys staying in and watching Game of Thrones with his girlfriend and learning about GatsbyJS</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Timothy Shores is a full-time full stack web development student at Lambda School. On Friday nights he enjoys staying in and watching Game of Thrones with his girlfriend and learning about GatsbyJS</p>
+            <ul>
+                <li>GitHub: <a href="https://github.com/timothyshores">@timothyshores</a></li>
+                <li>LinkedIn: <a href="https://www.linkedin.com/in/timothyshores/">Timothy Shores</a></li>
+                <li>Twitter: <a href="https://twitter.com/timothymshores">@timothymshores</a></li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Contact;
