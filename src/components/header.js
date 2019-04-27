477cd9acc92a9d331195a5457425dd893a31cb0a
Timothy Shores <timothymshores@gmail.com>
2019-04-27 14:33:39 -0500

    Renamed header.scss to header.module.scss, imported as headerStyles in header.js and updated className

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "gatsby"
-import './header.scss'
+import headerStyles from './header.module.scss'
 
 const Header = () => {
     return (
@@ -10,10 +10,10 @@ const Header = () => {
             </header>
             <nav>
                 <ul>
-                    <li><Link className="link" to="/">Home</Link></li>
-                    <li><Link className="link" to="/about">About</Link></li>
-                    <li><Link className="link" to="/blog">Blog</Link></li>
-                    <li><Link className="link" to="/contact">Contact</Link></li>
+                    <li><Link className={headerStyles.link} to="/">Home</Link></li>
+                    <li><Link className={headerStyles.link} to="/about">About</Link></li>
+                    <li><Link className={headerStyles.link} to="/blog">Blog</Link></li>
+                    <li><Link className={headerStyles.link} to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </div>
