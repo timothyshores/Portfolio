57563970c4adbee7269c9d4e638ec71a203e3439
Timothy Shores <timothymshores@gmail.com>
2019-04-27 00:09:13 -0500

    Create blog post with the url /blog

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>This page will display a list of blog posts</p>
+        </div>
+    )
+}
+
+export default Blog;
