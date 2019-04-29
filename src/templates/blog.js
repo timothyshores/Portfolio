cf62cb2942a82f1d15d493a21de53fd09c9e4279
Timothy Shores <timothymshores@gmail.com>
2019-04-29 10:58:42 -0500

    Write graphql query to fetch title, date and html from markdown files in /blog. Display fetched data in Layout component

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-const Blog = () => {
+export const query = graphql`
+query($slug: String!) {
+    markdownRemark (fields: {slug: { eq: $slug } }) {
+        frontmatter {
+            title
+            date(formatString: "MMMM Do, YYYY")
+        }
+        html
+    }
+}`
+
+const Blog = (props) => {
+    const { title, date } = props.data.markdownRemark.frontmatter
+    const { html } = props.data.markdownRemark
     return (
         <Layout>
-            This is the blog template
+            <h1>{title}</h1>
+            <p>{date}</p>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
         </Layout>
     )
 }
